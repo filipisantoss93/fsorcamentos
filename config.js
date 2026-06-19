8dfd9fb54fcee7d6d5c0ac8350819b4110313836
filipisantoss93 <filipi.199320@gmail.com>
2026-06-18 22:24:32 -0300

    Evita runtime duplicado em orcamentos

--- config.js
+++ config.js
@@ -183,8 +183,7 @@ const FS_CONFIG_SCRIPTS_POR_PAGINA = [
   {
     paginas: ['/orcamentos', '/orcamentos.html'],
     scripts: [
-      ['orcamentos-pdf.js', 'fs-orcamentos-pdf-js'],
-      ['orcamentos-resumo-grid-fix.js', 'fs-orcamentos-resumo-grid-fix-js']
+      ['orcamentos-pdf.js', 'fs-orcamentos-pdf-js']
     ]
   },
   {
